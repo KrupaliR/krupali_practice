1272a47af0f6b9575bd66dd82e873228db2a4875
krupalipinakinbhatt <krupalipinakinbhatt@krupalis-mbp.ht.home>
2022-06-10 19:57:32 -0400

    version 3

---
+++ Downloads/SYST17796_ProjectStarterCode 2/ca/sheridancollege/project/Deck.java
@@ -0,0 +1,20 @@
+package ca.sheridancollege.project;
+
+public class Deck {
+
+	private int size;
+
+	public void getSize() {
+		// TODO - implement Class.getSize
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param size
+	 */
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+}
---
+++ Downloads/SYST17796_ProjectStarterCode 2/src/ca/sheridancollege/project/Deck.java
@@ -0,0 +1,35 @@
+package ca.sheridancollege.project;
+
+public class Deck {
+
+	private int Getter;
+	private int attribute;
+
+	public void getGetter() {
+		// TODO - implement Deck.getGetter
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Getter
+	 */
+	public void setGetter(int Getter) {
+		// TODO - implement Deck.setGetter
+		throw new UnsupportedOperationException();
+	}
+
+	public void getAttribute() {
+		// TODO - implement Deck.getAttribute
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param attribute
+	 */
+	public void setAttribute(int attribute) {
+		this.attribute = attribute;
+	}
+
+}
